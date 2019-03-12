a9eb6d3a21861c499a77974454f2bbe5f62ec2bd
周君瑜 <928522038@qq.com>
2019-03-12 10:54:25 +0800

    wechat page

--- pageofnick/src/main/java/com/example/pageofnick/MainActivity.java
+++ pageofnick/src/main/java/com/example/pageofnick/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.page);
+        setContentView(R.layout.mainpage);
     }
 }
